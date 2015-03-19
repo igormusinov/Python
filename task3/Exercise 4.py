8c83d84dd0c034dcef2ef035a1f829042014c8e2
igormusinov <igormusinov@users.noreply.github.com>
2015-03-19 09:59:27 +0300

    Update Exercise 4.py

--- task3/Exercise 4.py
+++ task3/Exercise 4.py
@@ -13,7 +13,7 @@ def main():
     print '2.Which year each species had the largest population:', dict(zip(animals, year[population.argmax(axis=0)]))   
     print '3.Which species has the largest population for each year:', dict(zip(year, species[population.argmax(axis=1)]))    
     print '4.Which years any of the populations is above 50000:', year[[nump.any(population > 50000, axis=1)]]    
-    print '5.The top 2 years for each species when they had the lowest populations:', dict(zip(animals, year[populations.argsort(axis=0)[:2]].T))    
+    print '5.The top 2 years for each species when they had the lowest populations:', dict(zip(species, year[populations.argsort(axis=0)[:2]].T))    
     gradient = nump.gradient(hares)
     print '6.Compare the change in hare population and the number of lynxes. Check correlation:'     
     print " Lynxes vs. Gradient Hares correlation", np.corrcoef(lynxes, gradient)[0,1]
